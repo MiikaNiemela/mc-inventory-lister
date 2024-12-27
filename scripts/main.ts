4cd85a5814e13ee7a53c78449077f8e67c5c5707
Miika Niemelä <miika@niemelat.com>
2024-12-27 15:42:05 +0200

    add completed code from the example game. Nice baseline to start experimentation,

--- scripts/main.ts
+++ scripts/main.ts
@@ -14,6 +14,7 @@ import {
   MinecraftEntityTypes,
   MinecraftItemTypes,
 } from "@minecraft/vanilla-data";
+import Utilities from "./utils";
 
 const START_TICK = 100;
 const ARENA_X_SIZE = 30;
@@ -25,6 +26,10 @@ const ARENA_VECTOR_OFFSET: Vector3 = { x: ARENA_X_OFFSET, y: ARENA_Y_OFFSET, z: 
 
 // global variables
 let curTick = 0;
+let score = 0;
+let cottaX = 0;
+let cottaZ = 0;
+let spawnCountdown = 1;
 
 function initializeBreakTheTerracotta() {
   const overworld = world.getDimension(MinecraftDimensionTypes.Overworld);
@@ -65,20 +70,141 @@ function initializeBreakTheTerracotta() {
   }
 
   world.sendMessage("BREAK THE TERRACOTTA");
+  let airBlockPerm = BlockPermutation.resolve(MinecraftBlockTypes.Air);
+  let cobblestoneBlockPerm = BlockPermutation.resolve(MinecraftBlockTypes.Cobblestone);
+
+  if (airBlockPerm) {
+    Utilities.fillBlock(
+      airBlockPerm,
+      ARENA_X_OFFSET - ARENA_X_SIZE / 2 + 1,
+      ARENA_Y_OFFSET,
+      ARENA_Z_OFFSET - ARENA_Z_SIZE / 2 + 1,
+      ARENA_X_OFFSET + ARENA_X_SIZE / 2 - 1,
+      ARENA_Y_OFFSET + 10,
+      ARENA_Z_OFFSET + ARENA_Z_SIZE / 2 - 1
+    );
+  }
+
+  if (cobblestoneBlockPerm) {
+    Utilities.fourWalls(
+      cobblestoneBlockPerm,
+      ARENA_X_OFFSET - ARENA_X_SIZE / 2,
+      ARENA_Y_OFFSET,
+      ARENA_Z_OFFSET - ARENA_Z_SIZE / 2,
+      ARENA_X_OFFSET + ARENA_X_SIZE / 2,
+      ARENA_Y_OFFSET + 10,
+      ARENA_Z_OFFSET + ARENA_Z_SIZE / 2
+    );
+  }
 }
 
+/**
+ * Main game loop
+ */
 function gameTick() {
   try {
     curTick++;
-
     if (curTick === START_TICK) {
       initializeBreakTheTerracotta();
+    }
+
+    if (curTick > START_TICK && curTick % 20 === 0) {
+      // no terracotta exists, and we're waiting to spawn a new one.
+      if (spawnCountdown > 0) {
+        spawnCountdown--;
+
+        if (spawnCountdown <= 0) {
+          spawnNewTerracotta();
+        }
+      } else {
+        checkForTerracotta();
+      }
+    }
+
+    // spawn mobs at not crypto safe random intervals
+    // increase the interval as the score goes up
+    const spawnInterval = Math.ceil(200 / ((score + 1) / 3));
+    if (curTick > START_TICK && curTick % spawnInterval === 0) {
+      spawnMobs(MinecraftEntityTypes.Zombie);
+    }
+
+    if (curTick > START_TICK && curTick % 29 === 0) {
+      addObstacles(MinecraftBlockTypes.CherryLeaves);
     }
   } catch (e) {
     console.warn("Tick error: " + e);
   }
 
   system.run(gameTick);
+}
+
+function spawnNewTerracotta() {
+  const overworld = world.getDimension(MinecraftDimensionTypes.Overworld);
+
+  // create new terracotta
+  cottaX = Math.floor(Math.random() * (ARENA_X_SIZE - 1)) - (ARENA_X_SIZE / 2 - 1);
+  cottaZ = Math.floor(Math.random() * (ARENA_Z_SIZE - 1)) - (ARENA_Z_SIZE / 2 - 1);
+
+  world.sendMessage("Creating new terracotta!");
+  let block = overworld.getBlock(Vector3Utils.add(ARENA_VECTOR_OFFSET, { x: cottaX, y: 1, z: cottaZ }));
+
+  if (block) {
+    block.setPermutation(BlockPermutation.resolve(MinecraftBlockTypes.YellowGlazedTerracotta));
+  }
+}
+
+function checkForTerracotta() {
+  const overworld = world.getDimension(MinecraftDimensionTypes.Overworld);
+
+  let block = overworld.getBlock(Vector3Utils.add(ARENA_VECTOR_OFFSET, { x: cottaX, y: 1, z: cottaZ }));
+
+  if (block && !block.permutation.matches(MinecraftBlockTypes.YellowGlazedTerracotta)) {
+    // we didn't find the terracotta! set a new spawn countdown
+    score++;
+    spawnCountdown = 2;
+    cottaX = -1;
+
+    const scoreObjective = world.scoreboard.getObjective("score");
+    if (scoreObjective) {
+      let players = world.getAllPlayers();
+
+      for (let player of players) {
+        scoreObjective.setScore(player, score);
+      }
+    } else {
+      console.warn("Score objective not found");
+    }
+
+    world.sendMessage("You broke the terracotta! Creating new terracotta in a few seconds.");
+    cottaZ = -1;
+  }
+}
+
+function spawnMobs(mobType: MinecraftEntityTypes) {
+  const overworld = world.getDimension(MinecraftDimensionTypes.Overworld);
+
+  // spawn mobs = create 1-2 mobs
+  let spawnMobCount = Math.floor(Math.random() * 2) + 1;
+
+  for (let j = 0; j < spawnMobCount; j++) {
+    let zombieX = Math.floor(Math.random() * (ARENA_X_SIZE - 2)) - ARENA_X_SIZE / 2;
+    let zombieZ = Math.floor(Math.random() * (ARENA_Z_SIZE - 2)) - ARENA_Z_SIZE / 2;
+
+    overworld.spawnEntity(mobType, Vector3Utils.add(ARENA_VECTOR_OFFSET, { x: zombieX, y: 1, z: zombieZ }));
+  }
+}
+
+function addObstacles(blockType: MinecraftBlockTypes) {
+  const overworld = world.getDimension(MinecraftDimensionTypes.Overworld);
+  for (let i = 0; i < 10; i++) {
+    const leafX = Math.floor(Math.random() * (ARENA_X_SIZE - 1)) - (ARENA_X_SIZE / 2 - 1);
+    const leafY = Math.floor(Math.random() * 10);
+    const leafZ = Math.floor(Math.random() * (ARENA_Z_SIZE - 1)) - (ARENA_Z_SIZE / 2 - 1);
+
+    overworld
+      .getBlock(Vector3Utils.add(ARENA_VECTOR_OFFSET, { x: leafX, y: leafY, z: leafZ }))
+      ?.setPermutation(BlockPermutation.resolve(blockType));
+  }
 }
 
 system.run(gameTick);
